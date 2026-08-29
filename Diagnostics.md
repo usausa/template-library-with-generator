cabf4633dd116f5a538d2fa7ea9c001857d08560
usausa <machi.pon@gmail.com>
2026-08-29 09:18:11 +0900

    Update

--- Diagnostics.md
+++ Diagnostics.md
@@ -2,5 +2,5 @@
 
 | ID | Severity | Description | How to fix |
 |---|---|---|---|
-| TP0001 | ⚠️ Warning | Target method is not `static partial` | Declare the method as `static partial` |
-| TP0002 | ⚠️ Warning | Target method has parameters | Remove the parameters from the method |
+| TP0001 | ⚠️ Warning | `[CustomMethod]` method is not `static partial` | Declare the method as `static partial` |
+| TP0002 | ⚠️ Warning | `[CustomMethod]` method has parameters | Remove the parameters from the method |
--- Template.Library.Generator/Diagnostics.cs
+++ Template.Library.Generator/Diagnostics.cs
@@ -7,7 +7,7 @@ internal static class Diagnostics
     public static DiagnosticDescriptor InvalidMethodDefinition { get; } = new(
         id: "TP0001",
         title: "Invalid method definition",
-        messageFormat: "Method must be static partial. method=[{0}]",
+        messageFormat: "[CustomMethod] method must be static partial. method=[{0}]",
         category: "Usage",
         defaultSeverity: DiagnosticSeverity.Warning,
         isEnabledByDefault: true);
@@ -15,7 +15,7 @@ internal static class Diagnostics
     public static DiagnosticDescriptor InvalidMethodParameter { get; } = new(
         id: "TP0002",
         title: "Invalid method parameter",
-        messageFormat: "Method must not have parameters. method=[{0}]",
+        messageFormat: "[CustomMethod] method must not have parameters. method=[{0}]",
         category: "Usage",
         defaultSeverity: DiagnosticSeverity.Warning,
         isEnabledByDefault: true);
